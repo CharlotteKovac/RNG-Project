5537411c4f397db1a2e3045a78efa8a20772ee96
CharlotteKovac <charkova.1430@gmail.com>
2024-04-16 15:26:43 -0700

    Split Switches in 2.
    There are now 2 switch statements. One for games and one for the description of the game.

--- RNG.js
+++ RNG.js
@@ -3,26 +3,47 @@ userName ? console.log(`Hi there, ${userName}. Ready to play a game?`) : console
 let gameQuestion = 'What kind of game do you want to play?' //defined var gQ
 console.log(`${userName}, wants to know... ${gameQuestion}`); //c.l to call uN and gQ together
 let rNGRange = Math.floor(Math.random() * 6); //var rNGR to offer 5 choices
-console.log(rNGRange); //call out which number was pulled from the above
+//console.log(rNGRange); //call out which number was pulled from the above
 let gamesToPlay = ''; //var gTP left empty to be filled in by switch statement below
 switch (rNGRange) { //switch to pull from rNGR
     case 0:
-        gamesToPlay = 'Stardew Valley is great for cozy, chill moments.'; //gTP value 0 to Stardew Valley
+        gamesToPlay = 'Stardew Valley.'; //gTP value 0 to Stardew Valley
         break;
     case 1:
-        gamesToPlay = "Baldur's Gate 3, to customize characters and go on a thrilling adventure that rivals the trek to the kitchen..."; //gTP value 1 to BG3
+        gamesToPlay = "Baldur's Gate 3."; //gTP value 1 to BG3
         break;
     case 2:
-        gamesToPlay = 'RimWorld, not for the faint of heart. These colonists lives are in your hands.'; //gTP value 2 to RimWorld
+        gamesToPlay = 'RimWorld.'; //gTP value 2 to RimWorld
         break;
     case 3:
-        gamesToPlay = 'Timberborn, cause you get to play as beavers. Need I say more?'; //gTP value 3 to Timberborn
+        gamesToPlay = 'Timberborn.'; //gTP value 3 to Timberborn
         break;
     case 4:
-        gamesToPlay = 'Pokemon. To research and battle your way to the top!'; //gTP value 4 to Pokemon
+        gamesToPlay = 'Pokemon.'; //gTP value 4 to Pokemon
         break;
     case 5:
-        gamesToPlay = 'Dark Souls. Cause you sometimes want to burn the world.'; //gTP value 5 to Dark Souls
+        gamesToPlay = 'Dark Souls.'; //gTP value 5 to Dark Souls
 
 }
-console.log(`The ideal game for you is...${gamesToPlay}`); //pulls gTP value from rNGR along with description of game.
+let rand = Math.floor(Math.random() * 6);
+let descriptor = '';
+switch (rand) {
+    case 0:
+        descriptor = "Great for cozy, chill moments.";
+        break;
+    case 1:
+        descriptor = 'To customize characters and go on a thrilling adventure that rivals the trek to the kitchen...'
+        break;
+    case 2:
+        descriptor = "Not for the faint of heart. These colonist's lives are in your hands.";
+        break;
+    case 3:
+        descriptor = "Cause you get to play as beavers. Need I say more?";
+        break;
+    case 4:
+        descriptor = "To research and battle your way to the top!";
+        break;
+    case 5:
+        descriptor = "To burn that pesky world to the ground.";
+}
+console.log(`The ideal game for you is...${gamesToPlay}. This game can ${descriptor}.`); //pulls gTP value from rNGR, also pulls rand and descriptor
